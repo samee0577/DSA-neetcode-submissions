8bf3f669693a312b1ce3178fa4bbc2c235c4a1cf
samee0577 <shaikhsamee0577@gmail.com>
2026-06-29 23:51:25 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let left = 0;
+        let right = numbers.length - 1;
+        while (left < right) {
+            const currentSum = numbers[left] + numbers[right];
+            if (currentSum === target) {
+                return [left + 1, right + 1];
+            } else if (currentSum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return [left, right];
+    }
+}

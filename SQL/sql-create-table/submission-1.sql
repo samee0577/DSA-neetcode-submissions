dded5d418dcdaa65d6f1ae1790fb70db7bb5127a
samee0577 <shaikhsamee0577@gmail.com>
2026-07-08 13:43:52 +0530

    Add: sql-create-table - submission-1

---
+++ SQL/sql-create-table/submission-1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE videos(
+    id INTEGER,
+    name TEXT,
+    CREATED_AT DATE,
+    published BOOLEAN
+);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
